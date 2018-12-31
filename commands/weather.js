3211babb0ecf3485b4a869606d215f47ad82fde2
Darren <darren3981@gmail.com>
2018-12-31 14:20:20 -0500

    redid embed to rich embed

--- commands/weather.js
+++ commands/weather.js
@@ -2,6 +2,7 @@ const opencage = require('opencage-api-client');
 const DarkSky = require('dark-sky')
 const config = require("../config.json");
 const darksky = new DarkSky(config.darkkey);
+const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     if(!args[0]) return message.reply("give a location idiot");
@@ -30,59 +31,25 @@ exports.run = (client, message, args) => {
                 )
             .catch(console.log)// end of darksky
 
-        const embed = {
-            "title": place.formatted,
-            "description": forecast.currently.summary + ' | ' + forecast.daily.summary,
-            "url": "https://discordapp.com",
-            "color": 16389353,
-            "footer": {
-              "icon_url": "https://darksky.net/images/darkskylogo.png",
-              "text": "Powered by DarkSky: https://darksky.net/poweredby/"
-            },
-            "thumbnail": {
-                "url": "https://darksky.net/images/darkskylogo.png"
-            },
-            "fields": [
-              {
-                "name": "Current Temperature:",
-                "value": forecast.currently.temperature.toString(),
-                "inline": true
-              },
-              {
-                "name": "Feels Like:",
-                "value": forecast.currently.apparentTemperature.toString(),
-                "inline": true
-              },
-              {
-                "name": "Chance of Precipitation:",
-                "value": (forecast.currently.precipProbability * 100).toString() + "%",
-                "inline": true
-              },
-              {
-                "name": "Type of Precipitation:",
-                "value": forecast.currently.precipType ? forecast.currently.precipType : "N/A",//forecast.currently.precipType,
-                "inline": true
-              },
-              {
-                "name": "Humidity:",
-                "value": (forecast.currently.humidity * 100).toString() + "%",
-                "inline": true
-              },
-              {
-                "name": "Wind Speed:",
-                "value": (forecast.currently.windSpeed).toString() + "mph",
-                "inline": true
-              },
-              {
-                "name": "Alerts:",
-                "value": forecast.alerts ? forecast.alerts[0].title : "N/A"
-              }
-
-
-            ] 
-          };
-          message.channel.send({ embed });        
-        //message.channel.send(JSON.stringify(place.geometry)).catch(console.error);
+       
+          const embed = new Discord.RichEmbed()
+      .setColor(16389353)
+      .setTimestamp()
+      .setFooter(
+        "requested by " + message.author.username + " | Powered by Dark Sky",
+        message.author.displayAvatarURL
+      )
+      .setThumbnail("https://darksky.net/images/darkskylogo.png")
+      .setTitle(place.formatted)
+      .setDescription(forecast.currently.summary + ' | ' + forecast.daily.summary)
+      .addField("Current Temperature:",forecast.currently.temperature.toString(),true)
+      .addField("Feels Like:",forecast.currently.apparentTemperature.toString(),true)  
+      .addField("Chance of Precipitation:",(forecast.currently.precipProbability * 100).toString() + "%",true)
+      .addField("Type of Precipitation:",forecast.currently.precipType ? forecast.currently.precipType : "N/A",true)
+      .addField("Humidity:",(forecast.currently.humidity * 100).toString() + "%",true)  
+      .addField("Wind Speed:",(forecast.currently.windSpeed).toString() + "mph",true)  
+      .addField("Alerts:", forecast.alerts ? forecast.alerts[0].title : "N/A",true)
+    message.channel.send({ embed });
         }//bracket ending weather()
         weather();     
 }//bracket ending exports.run

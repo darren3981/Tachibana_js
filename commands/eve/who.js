1c5157099c6dda5e81b215b4deb2aadb684407d3
Darren <darren3981@gmail.com>
2019-04-23 23:32:34 -0500

    added template literals into call uris

--- commands/eve/who.js
+++ commands/eve/who.js
@@ -1,5 +1,6 @@
 const rp = require("request-promise");
 const Discord = require("discord.js");
+var moment = require("moment");
 async function search(name) {
   //search info
 
@@ -19,7 +20,7 @@ async function char_info(char_id) {
   //call char info with char_info()
 
   const search2 = {
-    uri: "https://esi.evetech.net/latest/characters/" + char_id
+    uri: `https://esi.evetech.net/latest/characters/${char_id}`
   };
   let response = rp(search2);
   let data2 = JSON.parse(await response);
@@ -30,7 +31,7 @@ async function corp_info(character) {
 
   let corp_id = character.corporation_id;
   const corp_search = {
-    uri: "https://esi.evetech.net/latest/corporations/" + corp_id
+    uri: `https://esi.evetech.net/latest/corporations/${corp_id}`
   };
   let response = rp(corp_search);
   let corp_info = JSON.parse(await response);
@@ -41,7 +42,7 @@ async function a_info(character) {
   try {
     let a_id = character.alliance_id;
     const a_search = {
-      uri: "https://esi.evetech.net/latest/alliances/" + a_id
+      uri: `https://esi.evetech.net/latest/alliances/${a_id}`
     };
     let response = rp(a_search);
     let a_info = JSON.parse(await response);
@@ -54,7 +55,7 @@ async function zstats(char_id) {
   //call char info with char_info()
 
   const zsearch = {
-    uri: "https://zkillboard.com/api/stats/characterID/" + char_id + "/"
+    uri: `https://zkillboard.com/api/stats/characterID/${char_id}/`
   };
   let response = rp(zsearch);
   let zstats = JSON.parse(await response);
@@ -63,14 +64,21 @@ async function zstats(char_id) {
 exports.run = async (client, message, args) => {
   if (!args[0]) return message.reply("you have to name a character idiot");
   let name = args.slice(0).join(" ");
+  let today = moment().format("YYYYMM");
   try {
     const char_id = await search(name);
     const character = await char_info(char_id);
     const corp_return = await corp_info(character);
     const a_return = await a_info(character);
-    const zinfo = await zstats(char_id)
-    zlink = "https://zkillboard.com/character/" + char_id;
-    evewholink = "https://evewho.com/pilot/" + encodeURIComponent(character.name)
+    const zinfo = await zstats(char_id);
+    zlink = `https://zkillboard.com/character/${char_id}`;
+    evewholink =
+      `https://evewho.com/pilot/${encodeURIComponent(character.name)}`;
+    let monthdata = zinfo.months[today] ? zinfo.months[today] : "0";
+    //console.log(monthdata)
+    let monthkills = monthdata.shipsDestroyed ? monthdata.shipsDestroyed : "0";
+    let monthdeaths = monthdata.shipsLost ? monthdata.shipsLost : "0";
+
     // embed
     const embed = new Discord.RichEmbed()
       .setColor(16389353)
@@ -81,7 +89,7 @@ exports.run = async (client, message, args) => {
       )
       .setAuthor(character.name)
       .setThumbnail(
-        "http://imageserver.eveonline.com/Character/" + char_id + "_128.jpg"
+        `http://imageserver.eveonline.com/Character/${char_id}_128.jpg`
       )
       .addField(
         "Corporation:",
@@ -95,12 +103,20 @@ exports.run = async (client, message, args) => {
           : "Character is not in an Alliance",
         false
       )
-      .addField("K/D:", zinfo.shipsDestroyed + " / " + zinfo.shipsLost, false)
-      .addField("Links:", "[zkill](" + zlink + ")" + "\n [evewho](" + evewholink + ")", false);
+      .addField("Monthly K/D:", monthkills + " / " + monthdeaths, false)
+      .addField(
+        "Alltime K/D:",
+        zinfo.shipsDestroyed + " / " + zinfo.shipsLost,
+        false
+      )
+      .addField(
+        "Links:",
+        "[zkill](" + zlink + ")" + "\n [evewho](" + evewholink + ")",
+        false
+      );
     message.channel.send({ embed });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     message.channel.send("character doesn't exist or you can't spell (lol)");
-    //client.users.get('104780607883599872').send('someone broke who');
   }
 };
